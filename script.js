bf66ce3aea75932817d36b926f9c1f6d6a2c659c
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2024-01-09 12:46:47 +0530

    upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let API_KEY = "AIzaSyAc2yIESuszzAW6WO_YrNbPPjewtlFWkJ8";
+let BASE_URL = "https://www.googleapis.com/youtube/v3";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
@@ -10,10 +15,7 @@ menuIcon.onclick = function()
   container.classList.toggle("large-container");
 }
 
-let API_KEY = "AIzaSyC08dTXZ8u4nGDkLLopBBuPvTEpiuEzrvY";
-let BASE_URL = "https://www.googleapis.com/youtube/v3";
-let video_http = "https://www.googleapis.com/youtube/v3/videos?";
-let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
 
 async function fetchVideos(maxResults) {
   let searchQuery;
@@ -24,6 +26,7 @@ async function fetchVideos(maxResults) {
  else
  {
   searchQuery="mostpopular";
+  sessionStorage.setItem("searchinput",searchQuery);
  }
   const response = await fetch(
     `${BASE_URL}/search?key=${API_KEY}&q=${searchQuery}&maxResults=${maxResults}&part=snippet`
@@ -42,7 +45,6 @@ async function fetchVideos(maxResults) {
 
 window.addEventListener("load",()=> 
 {
-  listcontainer.innerHTML = `<h1 class="loader">Videos are loading.....</h1>`;
   fetchVideos(20);
 });
 
@@ -70,18 +72,19 @@ const getChannelIcon = (video_data) => {
   .then((res) => res.json())
   .then((data) => {
     video_data.channelThunbnail = data.items[0].snippet.thumbnails.default.url;
-    rendervideo(video_data);
+    rendervideo(video_data,video_data.snippet.channelId);
   });
   
 };
 
-const playVideo = (vid) => {
+const playVideo = (vid,channelid) => {
   sessionStorage.setItem("videoId",vid);
+  sessionStorage.setItem("channelid",channelid);
   window.location.href = "videoplayer.html";
 };
 
 
-const rendervideo = (item) => {
+const rendervideo = (item,channelid) => {
   const mdiv = document.createElement("div");
          mdiv.className = "vid-list";
          mdiv.innerHTML = `<div class="vid-click" ><img src="${item.snippet.thumbnails.high.url}" class="thumbnail" alt="thumbnail"
@@ -97,8 +100,120 @@ const rendervideo = (item) => {
            </div>`;
            mdiv.addEventListener("click",()=>{
               console.log(item.id.videoId);
-                      playVideo(item.id.videoId);
+              if(sessionStorage.getItem("channelid"))
+              {
+                sessionStorage.removeItem("channelid");
+              }
+                      playVideo(item.id.videoId,channelid);
                 });
            listcontainer.appendChild(mdiv);
 };
+
+
+
+
+
+
+// function navigateToVideoDetails(videoId) {
+//   // document.cookie = `id=${videoId}; path=/videoplayer.html`;
+//   currentid = videoId;
+//   window.location.href = "http://127.0.0.1:5500/videoplayer.html";
+// }
+
+// function rendervideo(videoarr)
+// {
+//   //  const listcontainer = document.querySelector(".list-container");
+//    listcontainer.innerHTML = "";
+
+//    videoarr.forEach((item)=>{
+//        const mdiv = document.createElement("div");
+//        mdiv.className = "vid-list";
+//        console.log(item.id.videoId);
+//        mdiv.id = item.id.videoId;
+//        mdiv.innerHTML = `<div class="vid-click" ><img src="${item.snippet.thumbnails.high.url}" class="thumbnail" alt="thumbnail"
+//      />
+//      <div class="flex-div">
+//        <img src="${item.channelLogo}" alt="Channel Logo" />
+//        <div class="vid-info">
+//            <p>${item.snippet.description}</p>
+//          </div></div></div>
+//          <div class="vid-info-1">
+//            <p>${item.snippet.channelTitle}</p>
+//            <p>15k Views .  ${item.snippet.publishTime}</p>
+//          </div>`;
+//          mdiv.addEventListener("click",(e)=>{
+//           e.preventDefault();
+//           navigateToVideoDetails(item.id.videoId);
+//        });
+//      listcontainer.appendChild(mdiv);
+//    });
+
+// }
+
+// function fetchVideos(searchQuery,maxR){
+//     fetch(
+//     video_http + new URLSearchParams({
+//       part : "snippet,contentDetails,statistics,player", 
+//       q: searchQuery,   
+//       chart : "mostPopular",
+//       maxResults : maxR,
+//       regionCode : "IN",
+//       key : API_KEY,
+//     })
+// )
+// .then((res) => res.json())
+// .then((data)=>{
+//   listcontainer.innerHTML = "";
+//   data.items.forEach((item)=>{
+//     console.log(item);
+//      getChannelIcon(item);
+//   })
+// })
+// .catch((err) => console.log(err));
+// }
+
+//   fetch(
+//     video_http + new URLSearchParams({
+//       part : "snippet, contentDetails,statistics,player",
+//       q : 
+//       chart : "mostPopular",
+//       maxResults : 20,
+//       regionCode : "IN",
+//       key : API_KEY,
+//     })
+// )
+// .then((res) => res.json())
+// .then((data)=>{
+//   data.items.forEach((item)=>{
+//     console.log(item);
+//      getChannelIcon(item);
+//   })
+// })
+// .catch((err) => console.log(err));
+
+// async function getVideoStats(videoId){
+   
+//     const response = await fetch(`${BASE_URL}/videos?key=${API_KEY}&part=statistics&id=${videoId}`);
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+// // viewCount
+
+// async function getChannelLogo(channelId){
+   
+//     const response = await fetch(`${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`);
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+// // getChannelLogo("UCeVMnSShP_Iviwkknt83cww");
+
+// async function getComments(videoId){
+//     const response = await fetch(`${BASE_URL}/commentThreads?key=${API_KEY}&videoId=${videoId}&maxResults=25&part=snippet`);
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+// getComments('Kwu1yIC-ssg');
 
